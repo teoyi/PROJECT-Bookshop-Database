59dfb373abecf839fd41e04308fb8437a77ec433
teoyi <yipengteo@yahoo.com.sg>
2020-08-02 21:43:53 -0700

    Cleaned up the OOP styling

--- LIBback.py
+++ LIBback.py
@@ -4,56 +4,42 @@ import os.path
 class Database():
     # Create a db to house the inputted data
     def __init__(self, db):
-        connect = sqlite3.connect(db)
-        cursor = connect.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS lib (id INTEGER PRIMARY KEY, Title TEXT, Author TEXT, Year INTEGER, ISBN INTEGER)")
-        connect.commit()
-        connect.close()
+        self.connect = sqlite3.connect(db)
+        self.cursor = connect.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS lib (id INTEGER PRIMARY KEY, Title TEXT, Author TEXT, Year INTEGER, ISBN INTEGER)")
+        self.connect.commit()
 
     # Execute a command to add a row entry that contains all 4 var
     def add_entry(self, Title, Author, Year, ISBN):
-        connect = sqlite3.connect("lib.db")
-        cursor = connect.cursor()
-        cursor.execute("INSERT INTO lib VALUES (NULL,?,?,?,?)", (Title, Author, Year, ISBN))
-        connect.commit()
-        connect.close()
+        self.cursor.execute("INSERT INTO lib VALUES (NULL,?,?,?,?)", (Title, Author, Year, ISBN))
+        self.connect.commit()
 
     # Execute a command to delete a book based on the ISBN
     # Since ISBN is a unique value that is tied to the book
     def delete_entry(self, id):
-        connect = sqlite3.connect("lib.db")
-        cursor = connect.cursor()
-        cursor.execute("DELETE FROM lib WHERE id = ?", (id,))
-        connect.commit()
-        connect.close()
+        self.cursor.execute("DELETE FROM lib WHERE id = ?", (id,))
+        self.connect.commit()
 
     # Execute command to search for a specific part
     def search_entry(self, Title = "", Author = "", Year = "", ISBN = ""):
-        connect = sqlite3.connect("lib.db")
-        cursor = connect.cursor()
-        cursor.execute("SELECT * FROM lib WHERE Title = ? OR Author = ? or YEAR = ? or ISBN = ?", (Title, Author, Year, ISBN))
-        rows = cursor.fetchall()
-        connect.close()
+        self.cursor.execute("SELECT * FROM lib WHERE Title = ? OR Author = ? or YEAR = ? or ISBN = ?", (Title, Author, Year, ISBN))
+        rows = self.cursor.fetchall()
         return rows
 
     # Execute command to update a book based on ISBN
     def update_entry(self, id, Title, Author, Year, ISBN):
-        connect = sqlite3.connect("lib.db")
-        cursor = connect.cursor()
-        cursor.execute("UPDATE lib SET Title = ?, Author = ?, Year = ?, ISBN = ? WHERE id = ? ", (Title, Author, Year, ISBN, id))
-        connect.commit()
-        connect.close()
+        self.cursor.execute("UPDATE lib SET Title = ?, Author = ?, Year = ?, ISBN = ? WHERE id = ? ", (Title, Author, Year, ISBN, id))
+        self.connect.commit()
 
     # Execute command to get values from db
     def view_all(self):
-        connect = sqlite3.connect("lib.db")
-        cursor = connect.cursor()
-        cursor.execute("SELECT * FROM lib")
-        rows = cursor.fetchall()
-        connect.close()
+        self.cursor.execute("SELECT * FROM lib")
+        rows = self.cursor.fetchall()
         return rows
 
-
+    def __del__(self):
+        self.connect.close()
+        
 #create_table()
 # add_entry()
 # delete_entry()
